650bf058bc290d5b5b7279e5f58e7a731fb10710
wangbin <wangbin@nec.cn>
2018-05-02 15:18:58 +0800

    1

---
+++ InvoicingSystemAPI/DBDataModel/Product/ProductCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBDataModel
+{
+    public class ProductCategory
+    {
+        [Key]
+        public Guid categoryID { get; set; }
+        public string categoryName { get; set; }
+        public Guid parentID { get; set; }
+        public int depth { get; set; }
+        public int sort { get; set; }
+        public bool enable { get; set; }
+        public Guid cID { get; set; }
+        public DateTime ctime { get; set; }
+        public DateTime uptime { get; set; }
+        public Guid upID { get; set; }
+    }
+}
---
+++ InvoicingSystemAPI/DBDataModel/Product/ProductPropertyValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBDataModel
+{
+    public class ProductPropertyValue
+    {
+        [Key]
+        public Guid propertyValueID { get; set; }
+        public string propertyValue { get; set; }
+        public Guid fk_propertyNameID { get; set; }
+        public int sort { get; set; }
+        public bool enable { get; set; }
+        public Guid cID { get; set; }
+        public DateTime ctime { get; set; }
+        public DateTime uptime { get; set; }
+        public Guid upID { get; set; }
+    }
+}
