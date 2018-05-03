0f1f23c80787bb662de69d539dee3a111d402f0d
严卓熙 <573913919@qq.com>
2018-05-04 00:09:27 +0800

    1

---
+++ InvoicingSystemAPI/InvoicingSystemAPI/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using DBDataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InvoicingSystemAPI.Controllers
+{
+    public class TestController : BaseController
+    {
+        [Route("api/Test/TestMethod")]
+        [HttpGet]
+        public List<Role> Test()
+        {
+            return new List<Role> { new Role { roleName = "test1" } };
+        }
+    }
+}
